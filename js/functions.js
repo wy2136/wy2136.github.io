5c7484aedd51e483745b3488975d15b3dbe8279f
Wenchang Yang <wenchang@princeton.edu>
2023-11-27 12:05:26 -0500

    write_stamp

--- js/functions.js
+++ js/functions.js
@@ -125,6 +125,34 @@ function write_copyright(){
     var year = d.getFullYear();
     document.write('&copy; '  + year + ' Wenchang Yang')
 }
+//convert int to Roman numbers; used in write_stamp
+const intToRoman = num => {
+  const lookup = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1],
+  ];
+  return lookup.reduce((acc, [k, v]) => {
+    acc += k.repeat(Math.floor(num / v));
+    num = num % v;
+    return acc;
+  }, '');
+};
+function write_stamp(){
+    var d = new Date();
+    var year = d.getFullYear();
+    document.write('<img width="100px" src="https://avatars.githubusercontent.com/u/8202276"><br>'+ intToRoman(parseInt(year)))
+}
 function write_publications(ifile='./json/publications.json'){
     document.write('<ul class="list-unstyled">');
     // get data from publications.json
